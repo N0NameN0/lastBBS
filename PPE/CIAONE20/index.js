e3b9bd57a0bff7732c6191292b4a071cce1dee3e
alonetrio <alonetrio@yahoo.com>
2025-02-03 16:08:45 +0100

    Update module path to CIAONE20

---
+++ PPE/CIAONE20/index.js
@@ -0,0 +1,19 @@
+//--------------------------------
+// PARAMETERS
+//--------------------------------
+
+FOPEN;
+var LightBarColor = "@X4F";
+var unLightBarColor = "@X07";
+var MaxOneLine = 14;
+var EnterOneLinerPrompt =
+  "@X0FE@X0Cnt@X04er @X0FO@X0Cn@X04e@X0FL@X0Cin@X04er@X08? @X07";
+var PromptYPos = 9;
+var AbortedTxt =
+  "@X08                          ■@X04■@X0C■  @X0FO@X0Cn@X04e@X0FL@X0Cin@X04er @X0FAb@X0Cor@X04ted  @X0C■@X04■@X08■";
+var SavedTxt =
+  "@X08                           ■@X04■@X0C■  @X0FO@X0Cn@X04e@X0FL@X0Cin@X04er @X0FS@X0Cav@X04ed  @X0C■@X04■@X08■@X07";
+
+//--------------------------------
+// CODE / Do not change anything below here
+//--------------------------------
